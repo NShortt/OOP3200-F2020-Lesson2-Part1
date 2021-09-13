cd45d9d2b8765ae4bbbe719104ce8a371798fd2b
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2021-09-13 08:36:14 -0400

    Add project files.

---
+++ OOP3200-F2020-Lesson2-Part1/OOP3200-F2020-Lesson2-Part1.cpp
@@ -0,0 +1,9 @@
+// OOP3200-F2020-Lesson2-Part1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
