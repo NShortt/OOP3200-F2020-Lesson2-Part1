7a8664bdd2c45e0829d8de3c666a484269d2166a
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2021-09-13 09:33:57 -0400

    Tested class person in main

--- OOP3200-F2020-Lesson2-Part1/main.cpp
+++ OOP3200-F2020-Lesson2-Part1/main.cpp
@@ -5,9 +5,14 @@
  * Project: OOP3200-F2021-Lesson2-Part1
  * Description: Example code that was done by Following along with the class demo done by prof Tom Tsiliopoulos
  */
+
 #include <iostream>
+#include "Person.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Declare object of custom data type
+    Person nick("Nick", 23);
+
+    nick.SaysHello();
 }
